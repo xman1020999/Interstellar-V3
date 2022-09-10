a27c6d94cb04bb9b6aaf8665aae05ca529b7dcf4
bub <85169821+imbubbo@users.noreply.github.com>
2022-09-09 20:06:58 -0400

    v.0.1.0

--- js/index.js
+++ js/index.js
@@ -104,4 +104,39 @@ function openGame() {
     win.document.body.appendChild(iframe)
     }
 
+    function discServer1() {
+        var win = window.open() 
+        var url = "https://3.interstellardev.repl.co/apps/discord.html"
+        var iframe = win.document.createElement('iframe')
+        iframe.style.width = "100%  ";
+        iframe.style.height = "100%";
+        iframe.style.position = "fixed";
+        iframe.style.top = "0";
+        iframe.style.bottom = "0";
+        iframe.style.left = "0";
+        iframe.style.left = "0";
+        iframe.style.border = "none";
+        iframe.style.backgroundcolor = "black"
+        iframe.src = url
+        win.document.body.appendChild(iframe)
+        }
+
+        function discServer2() {
+            var win = window.open() 
+            var url = "https://5.interstellardev.repl.co/apps/discord.html"
+            var iframe = win.document.createElement('iframe')
+            iframe.style.width = "100%  ";
+            iframe.style.height = "100%";
+            iframe.style.position = "fixed";
+            iframe.style.top = "0";
+            iframe.style.bottom = "0";
+            iframe.style.left = "0";
+            iframe.style.left = "0";
+            iframe.style.border = "none";
+            iframe.style.backgroundcolor = "black"
+            iframe.src = url
+            win.document.body.appendChild(iframe)
+            }
+
+
 
