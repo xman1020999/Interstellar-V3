6f3688c49375adbbf00ef54bfb567f12ff6f65b2
bub <85169821+imbubbo@users.noreply.github.com>
2022-09-11 13:47:35 -0400

    v.0.1.1

--- js/index.js
+++ js/index.js
@@ -213,3 +213,20 @@ function discServer5() {
                         iframe.src = url
                         win.document.body.appendChild(iframe)
                         }
+
+                        function discServer6() {
+                            var win = window.open() 
+                            var url = "https://x.interstellardev.repl.co/apps/discord.html"
+                            var iframe = win.document.createElement('iframe')
+                            iframe.style.width = "100%  ";
+                            iframe.style.height = "100%";
+                            iframe.style.position = "fixed";
+                            iframe.style.top = "0";
+                            iframe.style.bottom = "0";
+                            iframe.style.left = "0";
+                            iframe.style.left = "0";
+                            iframe.style.border = "none";
+                            iframe.style.backgroundcolor = "black"
+                            iframe.src = url
+                            win.document.body.appendChild(iframe)
+                            }
