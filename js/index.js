42bd106d4f98a05938a40eb4a57787a797088407
Bubbo <85169821+imbubbo@users.noreply.github.com>
2022-10-21 22:29:31 -0400

    v3.1.8

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ if (!inFrame && !navigator.userAgent.includes("Firefox")) {
         const style = iframe.style
         const link = doc.createElement("link")
 
-        doc.title = "Bigfoot's Game Shack"
+        doc.title = "Google Classroom"
         link.rel = "icon";
         link.href = "https://raw.githubusercontent.com/BigfootsGS/BigfootsGS.github.io/main/images/favicon.ico";
         iframe.src = location.href
